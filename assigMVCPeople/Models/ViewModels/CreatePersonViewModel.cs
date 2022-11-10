14c4e5e641ab0fafea71b7786a807542e6120106
NataliaKarlsson <112874590+NataliaKarlsson@users.noreply.github.com>
2022-11-10 09:53:37 +0100

    created person view model

--- assigMVCPeople/Models/ViewModels/CreatePersonViewModel.cs
+++ assigMVCPeople/Models/ViewModels/CreatePersonViewModel.cs
@@ -1,7 +1,18 @@
-﻿namespace assigMVCPeople.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace assigMVCPeople.Models.ViewModels
 {
     public class CreatePersonViewModel
     {
         //What´s displayed
+        [Display(Name="Person")]
+        [Required]
+         public string?  Name { get; set; }
+         public double PhoneNumber { get; set; }
+        [Required]
+
+        public string? City { get; set; }
+
+        
     }
 }
