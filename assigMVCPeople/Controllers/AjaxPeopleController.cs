05f7994c08ce774db1aea646cfe80c4242b2bb34
NataliaKarlsson <112874590+NataliaKarlsson@users.noreply.github.com>
2022-11-11 13:06:38 +0100

    created AjaxPeople & Index

---
+++ assigMVCPeople/Controllers/AjaxPeopleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace assigMVCPeople.Controllers
+{
+    public class AjaxPeopleController : Controller
+    {
+        // GET: AjaxPeopleController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }   
+
+}
