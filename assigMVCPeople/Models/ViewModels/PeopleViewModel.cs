a55d45f7b5dd4102164d12122b30f8c4d30498ec
sita-Ax <sita81@hotmail.com>
2022-11-17 11:31:03 +0100

    create peopleviewmodel

---
+++ assigMVCPeople/Models/ViewModels/PeopleViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace assigMVCPeople.Models.ViewModels
+{
+    public class PeopleViewModel
+    {
+        public List<Person> PeopleListView { get; set; }
+        public string? FilterString { get; set; }
+
+        public PeopleViewModel()
+        {
+            PeopleListView = new List<Person>();
+        }
+    }
+}
