2a15a6e486bb7fb24e5112e78f6b5430b134cb12
sita-Ax <sita81@hotmail.com>
2022-12-14 14:37:17 +0100

    account, context, layout and imports

---
+++ assigMVCPeople/Models/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace assigMVCPeople.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public AppUser()
+        {
+
+        }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
