1cca24d16752e3de60687a27b0af52e00c09b710
NataliaKarlsson <112874590+NataliaKarlsson@users.noreply.github.com>
2022-12-15 11:23:15 +0100

    create RoleView

---
+++ assigMVCPeople/Models/ViewModels/ManageRolesViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace assigMVCPeople.Models.ViewModels
+{
+    public class ManageRolesViewModel
+    {
+        public IdentityRole Role { get; set; }
+        public IList<AppUser> UsersWithRole { get; set; }
+        public IList<AppUser> UsersNoRole { get; set; }
+
+    }
+}
